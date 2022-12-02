6d41abebe3abec504cf02dcba372c2b33fe55f07
FelipeAmorimDev <felipeamorim.go@gmail.com>
2022-12-02 01:07:33 -0300

    Adicionado modo pc

--- script/app.js
+++ script/app.js
@@ -1,46 +1,59 @@
 const categoriaPratos = document.querySelectorAll(".pratos__item__btn");
 const categoriaBebida = document.querySelectorAll(".bebida__btn")
+const categoriaSobremesa = document.querySelectorAll(".sobremesa__btn")
+let qntItemCarrinho = 0;
+
 categoriaPratos.forEach((item) => {
   item.addEventListener("click", selecionarItem)
 })
+categoriaBebida.forEach((item) => {
+  item.addEventListener("click", selecionarItem)
+})
+categoriaSobremesa.forEach((item) => {
+  item.addEventListener("click", selecionarItem)
+})
+
+
 
 function selecionarItem(event) {
-  let selected = pratoSelecionado()
-  if(selected.length === 0){
+  let selected;
+  selected = itemSelecionado(event)
+
+  if (selected.length === 0) {
     event.target.parentElement.classList.add("item-checked")
+    qntItemCarrinho++;
+    if(qntItemCarrinho===3){
+      carrinhoLiberar()
+    }
   }
-  else{
+  else {
     let eventId = event.target.parentElement.id;
     let selectId = selected[0].id
-    if(!(eventId === selectId)){
+    if (!(eventId === selectId)) {
       selected[0].classList.remove("item-checked")
       event.target.parentElement.classList.add("item-checked")
     }
-    
   }
   return event;
 }
 
-
-function pratoSelecionado() {
-  const pratoSection = document.querySelectorAll(".pratos__item--pratos")
+function itemSelecionado(classitem) {
+  let className = "." + classitem.target.parentElement.classList[0];
+  const itemElement = document.querySelectorAll(className)
   let itens = []
-  var teste = pratoSection.forEach((element, index) => {
-    if(element.classList.contains("item-checked")){
-      itens.push(pratoSection[index])
+  var teste = itemElement.forEach((element, index) => {
+    if (element.classList.contains("item-checked")) {
+      itens.push(itemElement[index])
     }
   })
   return itens;
 }
 
-function bebidaSelecionada() {
-  const bebidaSection = document.querySelectorAll(".pratos__item--bebida")
-  let itens = []
-  var teste = bebidaSection.forEach((element, index) => {
-    if(element.classList.contains("item-checked")){
-      itens.push(bebidaSection[index])
-    }
-  })
-  return itens;
+function carrinhoLiberar(){
+  const botaoPedido = document.querySelector(".botao button")
+  if(qntItemCarrinho===3){
+    botaoPedido.style.backgroundColor = "#32B72F";
+    botaoPedido.innerHTML = "Fechar Pedido"
+    botaoPedido.style.cursor = "pointer";
+  }
 }
-
